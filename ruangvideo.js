ff51727f5bf97b3f0790def069e227ca257f78cc
RaisaAdistra <rrsasikirana@gmail.com>
2022-11-10 21:24:44 +0700

    third commit bismillah terakhir

--- ruangvideo.js
+++ ruangvideo.js
@@ -1,11 +1,11 @@
 const videos = [
   {
     id: 1,
-    title: 'Apa Itu HKSR?',
+    title: 'Apa Itu HKSR/SRHR?',
     subTitle: 'Buat teman-teman yang mungkin bertanya-tanya HKSR itu apa sih? Kenapa Sudut Aman kok menggembar-gemborkan hal ini?',
     imageLink: 'https://img.freepik.com/free-vector/flat-people-group-asking-questions_23-2148927481.jpg?w=996&t=st=1668062838~exp=1668063438~hmac=f70b7b15cd09e7346c34b6588645afdcec5f8ce38ed40b346e545ea2710a83a3',
     content: '<p>tes tes tes bunyi hujan</p>',
-    videoLink: 'https://www.youtube.com/embed?v=snc-fAeslH4&pp=ugMICgJpZBABGAE%3D',
+    videoLink: 'https://www.youtube.com/embed/jsnusO0T2-0',
     createdAt: '22 Jan 2022',
     author: 'Youtube',
     imageAuthorLink: 'https://img.freepik.com/free-photo/people-emotions-lifestyle-leisure-beauty-concept-upbeat-happy-cheerful-asian-girl-dancing-having-fun-partying-moving-rhythm-music-smiling-yellow-background_1258-58916.jpg?w=996&t=st=1668062699~exp=1668063299~hmac=a3b80ff238e63e1cae2c1eb9a9172f5e4e73fce39d89e9b3455f2d997c6ee4e5'
@@ -16,7 +16,7 @@ const videos = [
     subTitle: 'Kenapa sih kalau sedang menstruasi seringnya muncul jerawat juga?',
     imageLink: 'https://img.freepik.com/free-vector/female-reproductive-system-with-flowers_52683-46660.jpg?w=996&t=st=1668062563~exp=1668063163~hmac=edb1448cfab2a51c7daea2077f5a29f7e0817b84ad433903c053751849d0ffb5',
     content: '<p> yayayayayaa</p>',
-    videoLink: 'https://youtu.be/embed/uFekBQ18CXU',
+    videoLink: 'https://www.youtube.com/embed/uFekBQ18CXU',
     createdAt: '22 Jan 2022',
     author: 'Youtube',
     imageAuthorLink: 'https://img.freepik.com/free-photo/people-emotions-lifestyle-leisure-beauty-concept-upbeat-happy-cheerful-asian-girl-dancing-having-fun-partying-moving-rhythm-music-smiling-yellow-background_1258-58916.jpg?w=996&t=st=1668062699~exp=1668063299~hmac=a3b80ff238e63e1cae2c1eb9a9172f5e4e73fce39d89e9b3455f2d997c6ee4e5'
@@ -27,7 +27,7 @@ const videos = [
     subTitle: 'Bagaimana cara kita bisa mengekspresikan diri sebagai diri kita sendiri?',
     imageLink: 'https://img.freepik.com/free-vector/youth-day-event-with-jumping-people_23-2148600297.jpg?w=996&t=st=1668062539~exp=1668063139~hmac=ba9a1a57d3b9982b66a193ed63b0f67dfd6658e939068562f961ac511e6798dc',
     content: '<p>lalalalala</p>',
-    videoLink: 'https://youtu.be/embed/ITRdvGnplLU',
+    videoLink: 'https://www.youtube.com/embed/ITRdvGnplLU',
     createdAt: '22 Jan 2022',
     author: 'Youtube',
     imageAuthorLink: 'https://img.freepik.com/free-photo/people-emotions-lifestyle-leisure-beauty-concept-upbeat-happy-cheerful-asian-girl-dancing-having-fun-partying-moving-rhythm-music-smiling-yellow-background_1258-58916.jpg?w=996&t=st=1668062699~exp=1668063299~hmac=a3b80ff238e63e1cae2c1eb9a9172f5e4e73fce39d89e9b3455f2d997c6ee4e5'
@@ -49,7 +49,7 @@ const videos = [
               <h2 class="text-center card-title">${element.title}</h2>
               <p class="text-center card-text my-4">${element.subTitle}</p>
               <div class="d-flex justify-content-center">
-              <a href ="${'./video.html?id=' + element.id}" class="btn btn-default" >Yuk Baca Selengkapnya</a>
+              <a href ="${'./video.html?id=' + element.id}" class="btn btn-default" >Yuk, tonton selengkapnya!</a>
               </div>
              
             </div>
